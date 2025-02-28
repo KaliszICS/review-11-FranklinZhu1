b952ab273844b89e4a98af2653318b803c10f9a1
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-02-28 14:28:16 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,36 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int[] createIntArray() {
+		int[] arr;
+		arr = new int[5];
+		for (int i = 0; i < 5; ++i) arr[i] = i + 1;
+		return arr;
 	}
 
+	public static String[] createArray(String a, String b, String c, String d) {
+		String[] arr;
+		arr = new String[4];
+		arr[0] = a;
+		arr[1] = b;
+		arr[2] = c;
+		arr[3] = d;
+		return arr;
+	}
+
+	public static int findValue(int n, int[] arr) {
+		for (int i = 0; i < arr.length; ++i) {
+			if (arr[i] == n) return i;
+		}
+		return -1;
+	}
+
+	public static int findThirdValue(String s, String[] arr) {
+		int count = 0;
+		for (int i = 0; i < arr.length; ++i) {
+			if (arr[i].equals(s)) ++count;
+			if (count == 3) return i;
+		}
+		return -1;
+	}
 
 }
